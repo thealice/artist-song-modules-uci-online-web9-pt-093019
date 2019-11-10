c8dd3ad468f232d7070885ad63fb65f2af7fafd7
Alice Freda <alicefreda@gmail.com>
2019-11-10 02:12:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
   extend Findable::ClassMethods
+  include Memorable::InstanceMethods
   include Paramable
   attr_accessor :name
   attr_reader :songs
